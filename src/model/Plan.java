27c19a66a51abc3d0f8611839bff611da73d5b10
Zu-za <zuza.jakab@gmail.com>
2019-08-25 12:27:26 +0200

    Plan added

---
+++ src/model/Plan.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Plan {
+    private static int currentID = 0;
+    private int planID;
+    private String name;
+    private int[] activitiesID;
+
+    boolean assignActivity(Activity activity){
+        return false;
+    }
+
+    boolean unassignActivity(Activity activity){
+        return false;
+    }
+}
