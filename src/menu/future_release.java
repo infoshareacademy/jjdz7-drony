b5abcae8a6031d0dd612f0a66dc2f8bf0b5b27b8
kklajbor <konrad.klajbor@gmail.com>
2019-09-01 14:01:18 +0200

    changes in structure, more content added

---
+++ src/menu/future_release.java
@@ -0,0 +1,24 @@
+package menu;
+
+public @interface future_release {
+
+
+}
+    private void printEndMenu(String endMenutype){
+        System.out.println("Menu --" + endMenutype + "--");
+        System.out.println("\n1. Zatwierdź");
+        System.out.println("2. Anuluj");
+        System.out.println("3. Zatwierdź i dodaj następny element");
+        System.out.println("4. Wyjście");
+    }
+    private void printUserEndMenu() {
+    }
+
+    private void printRoomEndMenu() {
+    }
+
+    private void printActivityEndMenu() {
+    }
+
+    private void printScheduleEndMenu() {
+    }
--- src/menu/menu.java
+++ src/menu/menu.java
@@ -4,136 +4,174 @@ import java.util.Scanner;
 
 public class menu {
     private boolean exit;
+    private boolean contextMenuExit;
+    private boolean subMenuExit;
 
     public void start() {
         initialParametersLoad();
         do {
             printMainMenu();
-            int choice =getUserInput();
+            int choice = getUserInput();
             mainMenuDecide(choice);
-        }while (!exit);
+        } while (!exit);
 // TODO discuss whether our app should save changes on the fly, or should it ask at the end of the program to write app state/config etd
     }
 
-    /** Menu choice logic **/
+    /**
+     * Menu choice logic
+     **/
 
     private void mainMenuDecide(int choice) {
-        switch (choice){
-            case 1:{
+        switch (choice) {
+            case 1: {
 //                searchMenu();
                 break;
             }
-            case 2:{
+            case 2: {
 //                usersMenu();
                 break;
             }
-            case 3:{
+            case 3: {
 //                roomsMenu();
                 break;
             }
-            case 4:{
+            case 4: {
 //                activitiesMenu();
                 break;
             }
-            case 5:{
+            case 5: {
+
 //                schedulesMenu();
                 break;
             }
-            case 6:{
+            case 6: {
                 exit = true;
                 break;
             }
-            default:{
+            default: {
                 printBadInputErrorMessage();
             }
 
         }
     }
 
-    private void searchMenu(){}
-    private void usersMenu(){}
-    private void roomsMenu(){}
-    private void activitiesMenu(){}
-    private void scheduleMenu(){}
+    private void searchMenu() {
 
-    /** Menu prints section **/
+        do {
+            printSearchMenu();
+            int choice = getUserInput();
+            searchMenuDecide(choice);
+        } while (!contextMenuExit);
+    }
 
-    private void printMainMenu(){}
-//    1.	Szukaj
-//    2.	Użytkownicy
-//    3.	Sale
-//    4.	Zajęcia
-//    5.	Plany
-//    6.	Wyjście
+    private void searchMenuDecide(int choice) {
+
+    }
+
+    private void usersMenu() {
+        do {
+            printContextMenu("Użytkownicy");
+            int choice = getUserInput();
+            usersMenuDecide(choice);
+        } while (!contextMenuExit);
+    }
+
+    private void usersMenuDecide(int choice) {
+
+    }
+
+    private void roomsMenu() {
+        printFeatureNotImplementedYet();
+//        printContextMenu("Sale");
+    }
+
+    private void activitiesMenu() {
+        do {
+            printContextMenu("Zajęcia");
+            int choice = getUserInput();
+            activitiesMenuDecide(choice);
+        } while (!contextMenuExit);
+    }
+
+    private void activitiesMenuDecide(int choice) {
+
+    }
+
+    private void schedulesMenu() {
+        do {
+            printContextMenu("Plany");
+            int choice = getUserInput();
+            schedulesMenuDecide(choice);
+        } while (!contextMenuExit);
+    }
+
+    private void schedulesMenuDecide(int choice) {
+
+    }
+
+    /**
+     * Menu prints section
+     **/
+
+    private void printMainMenu() {
+        System.out.println("\n1. Szukaj");
+        System.out.println("2.Użytkownicy");
+        System.out.println("3. Sale");
+        System.out.println("4. Zajęcia");
+        System.out.println("5. Plany");
+        System.out.println("6. Wyjście");
+    }
+
+    private void printSearchMenu() {
+        System.out.println("\n1. Szukaj po nazwie użytkownika");
+        System.out.println("2. Szukaj po nazwie sali");
+        System.out.println("3. Szukaj po nazwie zajęć");
+        System.out.println("4. Szukaj po nazwie planu");
+        System.out.println("5. Wyjście");
+    }
+
+    private void printContextMenu(String contextMenuType) {
+        System.out.println("Menu --" + contextMenuType + "--");
+        System.out.println("\n1. Dodaj");
+        System.out.println("2. Edytuj");
+        System.out.println("3. Usuń");
+        System.out.println("4. Pokaż");
+        System.out.println("5. Wyjście");
+    }
+
+    private void printUserSubMenu() {
 
 
-    private void printSearchMenu(){}
-
-//1.	Szukaj po nazwie użytkownika
-//2.	Szukaj po nazwie Sali
-//3.	Szukaj po nazwie zajęć
-//4.	Szukaj po nazwie planu
-//5.	Wyjście
-
-    private void printUserContextMenu(){}
-
-    /** TODO move 1-4 to one function to avoid repeating same println commands*/
-
-
-//    1.	Dodaj użytkowników / sale / zajęcia /plany w zależności od wyboru)
-//    2.	Edytuj
-//    3.	Usuń
-//    4.	Pokaż
-//    5.	Wyjście
-
-
-    private void printRoomContextMenu(){}
-
-//    1.	Dodaj użytkowników / sale / zajęcia /plany w zależności od wyboru)
-//    2.	Edytuj
-//    3.	Usuń
-//    4.	Pokaż
-//    5.	Wyjście
-
-    private void printActivityContextMenu(){}
-
-//    1.	Dodaj użytkowników / sale / zajęcia /plany w zależności od wyboru)
-//    2.	Edytuj
-//    3.	Usuń
-//    4.	Pokaż
-//    5.	Wyjście
-
-    private void printScheduleContextMenu(){}
-
-//    1.	Dodaj użytkowników / sale / zajęcia /plany w zależności od wyboru)
-//    2.	Edytuj
-//    3.	Usuń
-//    4.	Pokaż
-//    5.	Wyjście
-
-    private void printUserSubMenu(){}
+    }
 
 //    Podaj Imię
 //    Podaj nazwisko
 //    Podaj... TODO check all fields needed to get necessary input from user - applies to below 3 classes as well, since logic will be similar
 
-    private void printRoomSubMenu(){}
-    private void printActivitySubMenu(){}
-    private void printScheduleSubMenu(){}
+    private void printRoomSubMenu() {
+    }
 
-    private void printUserEndMenu(){}
-    private void printRoomEndMenu(){}
-    private void printActivityEndMenu(){}
-    private void printScheduleEndMenu(){}
+    private void printActivitySubMenu() {
+    }
 
-    private void printBadInputErrorMessage()
+    private void printScheduleSubMenu() {
+    }
+
+
+    private void printBadInputErrorMessage() {
+    }
+
+    private void printFeatureNotImplementedYet() {
+        System.out.println("Feature będzie wprowadzony w przyszłości");
+    }
+
     /**
      * placeholder
      */
 
     /**
      * UTIL PART OF THE CLASS.
-     *
+     * <p>
      * TODO - decide if this should be split to separate file
      */
 
@@ -143,16 +181,17 @@ public class menu {
         return Integer.parseInt(readUserInput());
     }
 
-    public static String readUserInput(){
+    public static String readUserInput() {
         return new Scanner(System.in).nextLine();
     }
 
-    public static String readInputWithMessage(String message){
+    public static String readInputWithMessage(String message) {
         System.out.println(message);
         return readUserInput();
     }
 
-    private void initialParametersLoad(){}
+    private void initialParametersLoad() {
+    }
 
 
 }
