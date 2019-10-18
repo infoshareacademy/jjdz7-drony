a976dcd36b6239725fed1f249d34aabcbd5e39a6
Patryk Zdanowicz <patrykzdan@gmail.com>
2019-10-18 23:17:59 +0200

    Java package change to java accroding to Jave EE requirements

--- Web/src/main/java/com.korpodrony/Servlet/IndexServlet.java
+++ Web/src/main/java/com.korpodrony/Servlet/IndexServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/hello")
+@WebServlet("/index")
 public class IndexServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
