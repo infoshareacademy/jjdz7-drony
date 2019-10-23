01b93d45f5df1c03f4b37527745cd1f5a0be948b
Patryk Zdanowicz <patrykzdan@gmail.com>
2019-10-23 19:34:01 +0200

    RepositoryService refactored

--- App/src/main/java/com/korpodrony/service/RepositoryService.java
+++ App/src/main/java/com/korpodrony/service/RepositoryService.java
@@ -15,40 +15,54 @@ import java.nio.file.Paths;
 import java.util.Set;
 
 public class RepositoryService {
-    private OrganizationRepositoryDao organization = new OrganizationRepositoryDaoImpl();
 
-    public void loadParametersFromFile() {
-        String path = new PropertiesService().getProperty(PropertiesService.APP_PATH);
-        Organization org = new Organization();
-        if (path == null) {
-            return;
-        } else {
-            if (Files.exists(Paths.get(path, "Users.json"))) {
-                Set<User> usersFromJson = new JSONReader().parseUserFromJSONFile(Paths.get(path, "Users.json"));
-                org.setUsers(usersFromJson);
-                User.setCurrentID(usersFromJson);
-            }
-            if (Files.exists(Paths.get(path, "Activities.json"))) {
-                Set<Activity> activtitiesFromJson = new JSONReader().parseActivityFromJSONFile(Paths.get(path, "Activities.json"));
-                org.setActivities(activtitiesFromJson);
-                Activity.setCurrentID(activtitiesFromJson);
-            }
-            if (Files.exists(Paths.get(path, "Plans.json"))) {
-                Set<Plan> plansFromJson = new JSONReader().parsePlanFromJSONFile(Paths.get(path, "Plans.json"));
-                org.setPlans(plansFromJson);
-                Plan.setCurrentID(plansFromJson);
-            }
-            OrganizationRepository.setOrganizationRepository(org);
-        }
-    }
+    private static final String USERS_JSON = "Users.json";
+    private static final String ACTIVITIES_JSON = "Activities.json";
+    private static final String PLANS_JSON = "Plans.json";
+    private OrganizationRepositoryDao organization = new OrganizationRepositoryDaoImpl();
 
     public void writeRepositoryToFile() {
         String path = new PropertiesService().getProperty(PropertiesService.APP_PATH);
         if (path == null) {
             return;
         }
-        JSONWriter.writeJSONToFile(Paths.get(path, "Users.json"), organization.getUsersSet());
-        JSONWriter.writeJSONToFile(Paths.get(path, "Activities.json"), organization.getActivitiesSet());
-        JSONWriter.writeJSONToFile(Paths.get(path, "Plans.json"), organization.getPlansSet());
+        JSONWriter.writeJSONToFile(Paths.get(path, USERS_JSON), organization.getUsersSet());
+        JSONWriter.writeJSONToFile(Paths.get(path, ACTIVITIES_JSON), organization.getActivitiesSet());
+        JSONWriter.writeJSONToFile(Paths.get(path, PLANS_JSON), organization.getPlansSet());
+    }
+
+    public void loadParametersFromFile() {
+        String path = new PropertiesService().getProperty(PropertiesService.APP_PATH);
+        Organization org = new Organization();
+        if (path != null) {
+            if (Files.exists(Paths.get(path, USERS_JSON))) {
+                setUsersFromFile(path, org);
+            }
+            if (Files.exists(Paths.get(path, ACTIVITIES_JSON))) {
+                setActivitiesFromFile(path, org);
+            }
+            if (Files.exists(Paths.get(path, PLANS_JSON))) {
+                setPlansFromFile(path, org);
+            }
+        }
+        OrganizationRepository.setOrganizationRepository(org);
+    }
+
+    private void setUsersFromFile(String path, Organization org) {
+        Set<User> usersFromJson = JSONReader.parseUserFromJSONFile(Paths.get(path, USERS_JSON));
+        org.setUsers(usersFromJson);
+        User.setCurrentID(usersFromJson);
+    }
+
+    private void setActivitiesFromFile(String path, Organization org) {
+        Set<Activity> activtitiesFromJson = JSONReader.parseActivityFromJSONFile(Paths.get(path, ACTIVITIES_JSON));
+        org.setActivities(activtitiesFromJson);
+        Activity.setCurrentID(activtitiesFromJson);
+    }
+
+    private void setPlansFromFile(String path, Organization org) {
+        Set<Plan> plansFromJson = JSONReader.parsePlanFromJSONFile(Paths.get(path, PLANS_JSON));
+        org.setPlans(plansFromJson);
+        Plan.setCurrentID(plansFromJson);
     }
 }
