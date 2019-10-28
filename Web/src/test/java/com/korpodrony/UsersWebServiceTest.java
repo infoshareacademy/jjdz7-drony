f9ec51c50aabcf0187abd115a70fa7a8d5ca3bf3
Patryk Zdanowicz <patrykzdan@gmail.com>
2019-10-28 18:51:35 +0100

    Navmenu changed to be showing actual possition

---
+++ Web/src/test/java/com/korpodrony/UsersWebServiceTest.java
@@ -0,0 +1,12 @@
+package com.korpodrony;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+class UsersWebServiceTest {
+
+    @Test
+    @Disabled
+    void getAllUsers() {
+    }
+}
