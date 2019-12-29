65edb2e41ba5670084e6ab8d18ef13072a9334c2
Patryk Zdanowicz <patrykzdan@gmail.com>
2019-12-29 11:17:40 +0100

    deploy.sh modificated

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,10 @@
 line=$(head -n 1 .env)
 arr=(${line//=/ })
 password=${arr[1]}
-file_path=./Web/src/main/resources/META-INF/persistence.xml
 file_path2=./Web/src/main/webapp/WEB-INF/web.xml
-orginal=$(sed '16q;d' $file_path)
 orginal2=$(sed '10q;d' $file_path2)
 new="            <property name=\"javax.persistence.jdbc.password\" value=\""$password"\"/>"
 new2="        <password>"$password"</password>"
-sed -i "s|$orginal|$new|g" $file_path
 sed -i "s|$orginal2|$new2|g" $file_path2
 
 echo "===== RUNNING MAVEN ====="
