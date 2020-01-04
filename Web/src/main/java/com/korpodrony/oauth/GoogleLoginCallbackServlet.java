54d136debf053941fddd7bdf5f18c403431d20b9
Patryk Zdanowicz <patrykzdan@gmail.com>
2020-01-04 21:56:35 +0100

    Deperecated method in OAuth2 modificiated, docker problem fixed

--- Web/src/main/java/com/korpodrony/oauth/GoogleLoginCallbackServlet.java
+++ Web/src/main/java/com/korpodrony/oauth/GoogleLoginCallbackServlet.java
@@ -5,19 +5,23 @@ import com.google.api.client.auth.oauth2.AuthorizationCodeResponseUrl;
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.servlet.auth.oauth2.AbstractAuthorizationCodeCallbackServlet;
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.http.HttpRequestInitializer;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.oauth2.Oauth2;
 import com.google.api.services.oauth2.model.Userinfoplus;
+import com.google.auth.http.HttpCredentialsAdapter;
+import com.google.auth.oauth2.AccessToken;
+import com.google.auth.oauth2.GoogleCredentials;
 import com.korpodrony.dto.UserDTO;
 import com.korpodrony.services.UsersWebService;
 
-import javax.ejb.EJB;
 import javax.inject.Inject;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
 import java.util.UUID;
 import java.util.logging.Logger;
 
@@ -33,20 +37,26 @@ public class GoogleLoginCallbackServlet extends AbstractAuthorizationCodeCallbac
     protected void onSuccess(HttpServletRequest req, HttpServletResponse resp, Credential credential)
             throws IOException {
 
-        GoogleCredential gCredential = new GoogleCredential()
-                .setAccessToken(credential.getAccessToken());
+        AccessToken accessToken = new AccessToken(credential.getAccessToken(), new Date(credential.getExpirationTimeMilliseconds()));
+
+        GoogleCredentials credentials = GoogleCredentials.newBuilder()
+                .setAccessToken(accessToken)
+                .build();
+
+        HttpRequestInitializer requestInitializer = new HttpCredentialsAdapter(credentials);
+
         Oauth2 oauth2 = new Oauth2.Builder(
                 new NetHttpTransport(),
                 JacksonFactory.getDefaultInstance(),
-                gCredential).setApplicationName("Korpodronny").build();
+                requestInitializer).setApplicationName("Korpodronny").build();
 
         Userinfoplus info = oauth2.userinfo().get().execute();
-        String name = info.getName();
-        String surname = info.getGivenName();
+        String name = info.getGivenName();
+        String surname = info.getFamilyName();
         String email = info.getEmail();
 
         if (usersWebService.findUserIdByEmail(email) == 0) {
-            usersWebService.createUser(name, "blank", email);
+            usersWebService.createUser(name, surname, email);
         }
 
         logger.info("Authentication success of user: " + name);
@@ -55,6 +65,7 @@ public class GoogleLoginCallbackServlet extends AbstractAuthorizationCodeCallbac
 
         req.getSession().setAttribute("email", verifiedUser.getEmail());
         req.getSession().setAttribute("name", verifiedUser.getName());
+        req.getSession().setAttribute("surname", verifiedUser.getName());
         req.getSession().setAttribute("userId", verifiedUser.getId());
 
 
