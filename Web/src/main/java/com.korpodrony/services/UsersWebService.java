12cd9c20be1f76e3193946023e34fc0ac79f3998
Patryk Zdanowicz <patrykzdan@gmail.com>
2020-01-11 18:58:02 +0100

    duplicated package removed

--- Web/src/main/java/com.korpodrony/services/UsersWebService.java
+++ Web/src/main/java/com.korpodrony/services/UsersWebService.java
@@ -2,14 +2,12 @@ package com.korpodrony.services;
 
 import com.korpodrony.daoInterfaces.UserRepositoryDaoInterface;
 import com.korpodrony.dto.UserDTO;
-import com.korpodrony.entity.UserEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RequestScoped
 public class UsersWebService {
